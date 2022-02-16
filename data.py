8cb4c756175483bc2218edc74d010d2a08d65dd4
Tae Kun Kim <taekunkim214@gmail.com>
2022-02-15 19:38:41 -0800

    implements data.py file

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+from torch.utils.data import DataLoader
 from numpy import genfromtxt
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader, random_split
@@ -5,29 +6,33 @@ from torchvision import transforms
 import torch
 
 ########## DO NOT change this function ##########
-# If you change it to achieve better results, we will deduct points. 
+# If you change it to achieve better results, we will deduct points.
+
+
 def train_val_split(train_dataset):
     train_size = int(len(train_dataset) * 0.8)
     val_size = len(train_dataset) - train_size
-    train_subset, val_subset = random_split(train_dataset, [train_size, val_size], 
+    train_subset, val_subset = random_split(train_dataset, [train_size, val_size],
                                             generator=torch.Generator().manual_seed(42))
     return train_subset, val_subset
 #################################################
 
+
 ########## DO NOT change this variable ##########
-# If you change it to achieve better results, we will deduct points. 
+# If you change it to achieve better results, we will deduct points.
 transform_test = transforms.Compose([
-  transforms.CenterCrop(224),
-  transforms.ToTensor(),
-  transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
 )
 #################################################
+
 
 class FoodDataset(Dataset):
     def __init__(self, data_csv, transforms=None):
         self.data = genfromtxt(data_csv, delimiter=',', dtype=str)
         self.transforms = transforms
-        
+
     def __getitem__(self, index):
         fp, _, idx = self.data[index]
         idx = int(idx)
@@ -39,22 +44,31 @@ class FoodDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
-def get_dataset(csv_path, transform):
-    return FoodDataset(csv_path, transform)
+    def get_dataset(csv_path, transform):
+        return FoodDataset(csv_path, transform)
 
-def create_dataloaders(train_set, val_set, test_set, args=None):
-    raise NotImplementedError()
+    def create_dataloaders(train_set, val_set, test_set, args=None):
+        train_dataloader = DataLoader(train_set, batch_size=64, shuffle=True)
+        val_dataloder = DataLoader(val_set, batch_size=64, shuffle=True)
+        test_dataloader = DataLoader(test_set, batch_size=64, shuffle=True)
 
-def get_dataloaders(train_csv, test_csv, args=None):
-    raise NotImplementedError()
-    train_dataset = 42
+        return train_dataloader, val_dataloder, test_dataloader
 
-########## DO NOT change the following two lines ##########
-# If you change it to achieve better results, we will deduct points. 
-    test_dataset = get_dataset(test_csv, transform_test)
-    train_set, val_set = train_val_split(train_dataset)
-###########################################################
+    def get_dataloaders(train_csv, test_csv, args=None):
+        train_set = self.get_dataset(train_csv, self.transform)
+        test_set = self.get_dataset(test_csv, self.transform)
 
-    dataloaders = None, None, None
-    return dataloaders
+        # splits train_set to train_set and val_set
+        train_subset, val_subset = train_val_split(train_set)
 
+        train_dataloader, val_dataloder, test_dataloader = create_dataloaders(
+            train_subset, val_subset, test_set, args)
+
+    ########## DO NOT change the following two lines ##########
+    # If you change it to achieve better results, we will deduct points.
+        test_dataset = get_dataset(test_csv, transform_test)
+        train_set, val_set = train_val_split(train_dataset)
+    ###########################################################
+
+        dataloaders = train_dataloader, val_dataloder, test_dataloader
+        return dataloaders
